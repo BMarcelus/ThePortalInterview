16efe27d56b4b9171206dbc5b72dbf6ce4f189da
BMarcelus <bmdizon@sbcglobal.net>
2017-03-03 13:09:32 -0800

    Finished upload
    finished implementing str for events

--- task2.js
+++ task2.js
@@ -12,22 +12,22 @@
 		 since the prompt says to create a data structure that contians/represents all of the information
 */
 
+var monthAbbr = ["Jan", "Feb", "Mar", "April", "May" ,"June", "July", "Aug" , "Sep", "Oct", "Nov", "Dec"];
 
-
-function CallendarDate(month, day, year)
+function CalendarDate(month, day, year)
 {
 	this.month = month;
 	this.day = day;
 	this.year = year;
 }
-CallendarDate.prototype = {
+CalendarDate.prototype = {
 	equals: function(other)
 	{
 		return this.month==other.month && this.day==other.day && other.year==this.year;
 	},
 	str: function()
 	{
-		
+		return monthAbbr[this.month-1] + " " + this.day + ", " + this.year; 
 	}
 }
 
@@ -42,17 +42,19 @@ function Event(name, date, start, end)
 Event.prototype = {
 	conflictsWith: function(other)
 	{
-		return this.date.equals(other.date) && ( (other.start >= this.start && other.start <= this.end) || (other.end >= this.start && other.end <= this.end));
+		return this.date.equals(other.date) && ( (other.start >= this.start && other.start < this.end) || (other.end > this.start && other.end <= this.end));
 	},
 	str: function()
 	{
-		return name+ ": " + date.str() + " " + timeFromInt(start) + " - " + timeFromInt(end);
+		return this.name+ ": " + this.date.str() + " " + timeFromInt(this.start) + " - " + timeFromInt(this.end);
 	}
 }
 
 function intFromTime(hours, minutes, pm)
 {
-	return (hours+12*pm)*60 + minutes;
+	var result =  (hours+12*pm)*60 + minutes;
+
+	return result;
 }
 function timeFromInt(totalMins)
 {
@@ -63,7 +65,9 @@ function timeFromInt(totalMins)
 	{
 		hours-=12;
 	}
-	return hours+":"+totalMins+(pm ? "PM" : "AM");
+	if(minutes==0)minutes="00";
+	else if(minutes<10)minutes = "0"+minutes;
+	return hours+":"+minutes+(pm ? "PM" : "AM");
 }
 
 
@@ -88,19 +92,25 @@ function overlappingEvents(events)
 
 var main = function(){
 	var events = [
-		new Event("Interview at The Portal", new CallendarDate(2, 23, 2017), intFromTime(3,00,1), intFromTime(4,30,1)),
-		new Event("Lunch with Cindy", new CallendarDate(2, 25, 2017), intFromTime(12,00,0), intFromTime(1,00,1)),
-		new Event("Dinner with John", new CallendarDate(2, 24, 2017), intFromTime(5,0,1), intFromTime(5,30,1)),
-		new Event("Conference", new CallendarDate(2, 24, 2017), intFromTime(11,0,0), intFromTime(5,30,1)),
+		new Event("Interview at The Portal", new CalendarDate(2, 23, 2017), intFromTime(3,00,1), intFromTime(4,30,1)),
+		new Event("Lunch with Cindy", new CalendarDate(2, 25, 2017), intFromTime(12,00,0), intFromTime(1,00,1)),
+		new Event("Dinner with John", new CalendarDate(2, 24, 2017), intFromTime(5,0,1), intFromTime(5,30,1)),
+		new Event("Conference", new CalendarDate(2, 24, 2017), intFromTime(11,0,0), intFromTime(5,30,1)),
 	];
 
-	events.push(new Event("Program", new CallendarDate(3,3,2017), intFromTime(6,0,1), intFromTime(10,0,1)));
-	events.push(new Event("Do Homework", new CallendarDate(3,4,2017), intFromTime(10,0,0), intFromTime(12,0,0)));
-	events.push(new Event("Sleep", new CallendarDate(3,4,2017), intFromTime(0,0,0), intFromTime(11,0,0)));
-	events.push(new Event("Do Nothing", new CallendarDate(3,4,2017), intFromTime(11,0,0), intFromTime(4,0,1)));
+	events.push(new Event("Program", new CalendarDate(3,3,2017), intFromTime(6,0,1), intFromTime(10,0,1)));
+	events.push(new Event("Do Homework", new CalendarDate(3,4,2017), intFromTime(10,0,0), intFromTime(12,0,0)));
+	events.push(new Event("Sleep", new CalendarDate(3,4,2017), intFromTime(0,0,0), intFromTime(11,0,0)));
+	events.push(new Event("Do Nothing", new CalendarDate(3,4,2017), intFromTime(11,0,0), intFromTime(4,0,1)));
+
 
 	var overlaps = overlappingEvents(events);
-	console.log(overlaps);
+
+	// console.log(overlaps);
+	for(var i in overlaps)
+	{
+		console.log(overlaps[i].str());
+	}
 
 }
 
